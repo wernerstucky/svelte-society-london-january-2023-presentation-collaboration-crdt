a67a6294262d2329b1cb373b79d656a80300951d
Werner Stucky <werner@stucky-solutions.net>
2023-01-14 13:10:07 +0200

    testing collab

--- src/routes/slides/demo1/+page.svelte
+++ src/routes/slides/demo1/+page.svelte
@@ -1,1 +1,12 @@
-demo 1
+<script>
+  import { svelteStore } from "./store.js";
+  import { JSONEditor } from "svelte-jsoneditor";
+
+  function handleChange(updatedContent) {
+    svelteStore.set(updatedContent);
+  }
+</script>
+
+<h1>JSON Editing</h1>
+
+<JSONEditor content={$svelteStore} onChange={handleChange} />
