72f0aa43574a2369bb9d264d95021efa27a212fc
Werner Stucky <werner@stucky-solutions.net>
2023-01-23 05:32:10 +0200

    small changes

--- src/routes/slides/defining/+page.svelte
+++ src/routes/slides/defining/+page.svelte
@@ -5,6 +5,7 @@
     <li>State on the browser = state on the server</li>
     <li>No save buttons</li>
     <li>Real-time updates</li>
+    <li>Zero API endpoints</li>
     <li>No need to refresh the page to hydrate</li>
   </ul>
 </div>
--- src/routes/slides/references/+page.svelte
+++ src/routes/slides/references/+page.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col items-center justify-center h-auto">
   <h1 class="text-6xl text-bold text-slate-800">References</h1>
   <br /><br />
-  <ul>
+  <ul class="text-2xl underline text-blue-600">
     <li><a href="https://crdt.tech/">crdt.tech</a></li>
     <li><a href="https://yjs.dev/">YJs</a></li>
     <li><a href="https://discuss.yjs.dev/">YJs Community - get involved!</a></li>
