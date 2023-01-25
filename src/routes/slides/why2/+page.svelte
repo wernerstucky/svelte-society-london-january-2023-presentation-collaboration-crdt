8899b63fa6a251d94fcd651dfc94d3e2c4cf6dc4
Werner Stucky <werner@stucky-solutions.net>
2023-01-25 04:28:32 +0200

    update pages

--- src/routes/slides/why2/+page.svelte
+++ src/routes/slides/why2/+page.svelte
@@ -1,11 +1,11 @@
 <div class="flex flex-col items-center justify-center h-auto">
   <h1 class="text-6xl font-bold text-slate-700 ">Ingredients for Fun</h1>
   <br /><br />
-  <ul class="text-3xl">
+  <ol class="text-3xl list-decimal">
     <li>Playfulness - freedom to fail</li>
     <li>Flow - engaging, on the edge of ability is best</li>
     <li>Connection - done in groups</li>
-  </ul>
+  </ol>
   <br />
   <a
     href="https://youtu.be/iMBJrvEwv8s?t=281"
